1606ce4ec926b0220ac4bb6908a0c76175f23d6b
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-03-31 05:35:25 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 const projectId = '68798d4af3a14bc3a05c33c3245d6a89'
+const keyData = fs.readFileSync('./p-key.txt', {
+  encoding: 'utf8', flag: 'r'
+})
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,11 +12,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [keyData]
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${projectId}`,
-      accounts: []
+      accounts: [keyData]
     }
   },
   solidity: {
